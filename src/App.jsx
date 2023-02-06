1a7a4e6d4af7640f8d083e3fb17db0e0dd18bbaa
Vinicius Goncalves <viniciusfod@gmail.com>
2023-02-06 19:04:51 -0300

    projeto concluido

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [list, setList] = useState([])
+  const [redoList, setRedoList] = useState([])
+
+  const handleClick = (event) => {
+    const newDot = {
+      x : event.clientX,
+      y : event.clientY
+    }
+
+    setList((prev) => [...prev, newDot])
+  }
+
+  const handleClickUndo = (event) => {
+    event.stopPropagation()
+
+    if (list.length === 0) return
+
+    const lastDot = list[list.length - 1]
+    setRedoList((prev) => [...prev, lastDot])
+
+    setList((prev) => {
+      const newList = [...prev].slice(0, -1)
+      return newList
+    })
+  };
+
+  const handleClickRedo = (event) => {
+    event.stopPropagation()
+
+    if (redoList.length === 0) return
+
+    const lastDot = redoList[redoList.length - 1]
+
+    setRedoList((prev) => {
+      const newArray = [...prev].slice(0, -1)
+      return newArray
+    })
+
+    setList((prev) => [...prev, lastDot])
+
+  }
+
+  const handleClickDell = (event) => {
+    event.stopPropagation()
+
+    setList([])
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="page" onClick={handleClick}>
+      {list.map((dot) => (
+        <span id="dot" key={dot.x} style={{top: dot.y, left: dot.x}}/>
+      ))}
+      <div id='btndiv'>
+        <button id='button' onClick={handleClickUndo}>Desfazer</button>
+        <button id='button' onClick={handleClickRedo}>Refazer</button>
+        <button id='button' onClick={handleClickDell}>Desfazer Tudo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
